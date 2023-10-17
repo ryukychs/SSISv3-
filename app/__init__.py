5ee34da32645aa6b945eae801e3d243b40864f77
ryukjiyu <dianne.tabanas@g.msuiit.edu.ph>
2023-10-17 21:14:24 +0700

    added student, course,  and college.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_mysql_connector import MySQL
 from flask_bootstrap import Bootstrap
 from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
@@ -21,8 +21,8 @@ def create_app(test_config=None):
     mysql.init_app(app)
     CSRFProtect(app)
 
-    from .user import user_bp as user_blueprint
-    app.register_blueprint(user_blueprint)
-
-
+    @app.route('/')
+    def homepage():
+        return render_template('index.html')
+    
     return app
